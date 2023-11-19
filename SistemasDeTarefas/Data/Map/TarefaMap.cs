3ea014c61b6e3f390c9123b392829091baa32b1c
Vincy de Tarsis <vincydetarsis@gmail.com>
2023-11-19 20:53:18 -0300

    Primeira Aula
    Essa foi a primeira aula completa com toda a API funcionando

---
+++ SistemasDeTarefas/Data/Map/TarefaMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Data.Map
+{
+    public class TarefaMap : IEntityTypeConfiguration<TarefaModel>
+    {
+        public void Configure(EntityTypeBuilder<TarefaModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(225);
+            builder.Property(x => x.Descricao).IsRequired().HasMaxLength(1000);
+            builder.Property(x => x.Status).IsRequired();
+        }
+    }
+}
---
+++ SistemasDeTarefas/Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Data.Map;
+using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            : base(options)
+        {
+        }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefa { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
